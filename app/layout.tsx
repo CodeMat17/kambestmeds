0b185db1a61ae0cc99fb27babe17ee52d39a1d73
CodeMat17 <codemat.biz@gmail.com>
2026-09-17 02:37:21 +0100

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,24 @@ export default async function RootLayout({
       lang='en'
       suppressHydrationWarning
       className={`${nunito.variable} h-full antialiased`}>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=AW-18456249465'
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18456249465');
+`,
+          }}
+        />
+      </head>
       <body className='min-h-full flex flex-col font-sans'>
         <ClerkProvider>
           <ThemeProvider
